aad1293e45a2c5c918a3c202d13fb14803a4713d
Medusa <78606636+MarianiFrancesco@users.noreply.github.com>
2024-04-29 15:28:33 +0200

    Update tunnel_in_proxy.go

--- share/tunnel/tunnel_in_proxy.go
+++ share/tunnel/tunnel_in_proxy.go
@@ -6,8 +6,8 @@ import (
 	"net"
 	"sync"
 
-	"github.com/jpillora/chisel/share/cio"
-	"github.com/jpillora/chisel/share/settings"
+	"github.com/MarianiFrancesco/VialeAlberato/share/cio"
+	"github.com/MarianiFrancesco/VialeAlberato/share/settings"
 	"github.com/jpillora/sizestr"
 	"golang.org/x/crypto/ssh"
 )
